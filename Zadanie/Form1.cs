9f07b785202a403f9ed82b61de2fb03d45053faa
Visual Student <visualstudent@zschie.edu.pl>
2022-10-03 09:04:59 +0200

    pt3

--- Zadanie/Form1.cs
+++ Zadanie/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Zadanie
 {
@@ -15,6 +17,13 @@ namespace Zadanie
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void start()
+        {
+            infbtn.Checked = true;
+            zmianazawodu();
+            pierwszybtn.Checked = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -35,9 +44,9 @@ namespace Zadanie
             string mail = textBox11.Text;
 
 
-            if (comboBox1.Text==x)
+            if (comboBox1.Text == x)
             {
-                tekst.Text += "Deklaruję przystąpienie do egzaminu potwierdzającego kwalifikacje w zawodzie przeprowadzonego w terminie" +" " + x +"\n\n";
+                tekst.Text += "Deklaruję przystąpienie do egzaminu potwierdzającego kwalifikacje w zawodzie przeprowadzonego w terminie" + " " + x + "\n\n";
             }
 
             else
@@ -46,14 +55,76 @@ namespace Zadanie
             }
 
 
-            tekst.Text += "Dane osobowe ucznia\n Nazwisko:       "+nazwisko +"\n Imię(imiona):     "+imie +"\n Data i miejsce urodzenia:    "+data+", "+miejsce +"\n Numer PESEL:       "+ pesel;
+            tekst.Text += "Dane osobowe ucznia\n Nazwisko:       " + nazwisko + "\n Imię(imiona):     " + imie + "\n Data i miejsce urodzenia:    " + data + ", " + miejsce + "\n Numer PESEL:       " + pesel;
+            tekst.Text += "\n Adres korespondencyjny\n miejscowość: " + miejscowosc + "\n ulica i numer domu: " + ulica + "\n kod pocztowy i poczta: " + kod + ", " + poczta + "\n nr telefonu z kierunkowym" + tel + "\n mail: " + mail;
             
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             var tekst = richTextBox1;
             tekst.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Pliki tekstowe .txt|*.txt|Wszystkie pliki *.*|*.*";
+            saveFileDialog1.Title = "Zapisz zawartość";
+            saveFileDialog1.DefaultExt = "txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter plik = new StreamWriter(saveFileDialog1.FileName, false);
+                plik.WriteLine(richTextBox1.Text);
+                plik.Close();
+            }
+        }
+
+
+        void infolabelu()
+        {
+            if (comboBox2.Text == "INF.02")
+                label13.Text = "Administracja i eksploatacja systemów komputerowych, \nurządzeń peryferyjnych i lokalnych sieci komputerowych";
+            else if (comboBox2.Text == "INF.03")
+                label13.Text = "Tworzenie i administrowanie stronami i aplikacjami \ninternetowymi oraz bazami danych";
+            else
+                label13.Text = "Projektowanie, programowanie i testowanie aplikacji";
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            infolabelu();
+        }
+
+
+        void zmianazawodu()
+        {
+            if (radioButton1.Checked == true)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("INF.02");
+                comboBox2.Items.Add("INF.03");
+                comboBox2.Text = "INF.02";
+            }
+            else
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("INF.03");
+                comboBox2.Items.Add("INF.04");
+                comboBox2.Text = "INF.03";
+                infolabelu();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            zmianazawodu();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            zmianazawodu();
         }
     }
 }
